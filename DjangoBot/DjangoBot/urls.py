b5a1d51d3fe1db8773128b8fe24fd71f27817e3e
Turishchev <pesntv05@gmail.com>
2024-12-02 22:23:30 +0500

    добаленны и настроены ссылки на стили  и js на главную страницу

--- DjangoBot/DjangoBot/urls.py
+++ DjangoBot/DjangoBot/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('globalwin/', include('globalwin.urls')),
+    #path('', include('main.urls')),
+    path('', include('globalwin.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
